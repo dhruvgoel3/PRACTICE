67e237e56a68722a9e506cbdadb135081a2ed39d
RepoByDhruv <dhruvgoel1100@gmail.com>
2024-05-05 11:36:17 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+//         byte b=127;
+//         short s = 300;
+//         int i = 100;
+//         float f = 35.3f;
+//         char c = 'A';
+
+//        System.out.println(b);
+//        System.out.println(s);
+//        System.out.println(i);
+//        System.out.println(f);
+//        System.out.println(c);
+
+
+
+//        byte b1 = 10;
+//        byte b2 = 0b1010;
+//        byte b3 =  012;
+//        byte b4 = 0XA;
+//        System.out.println(b1);
+//        System.out.println(b2);
+//        System.out.println(b3);
+//        System.out.println(b4);
+//
+
+//        long l =  999999999999L;
+//        System.out.println(l);
+
+        float f = 12.56f;
+        double d = 12.56;
+        System.out.println(f);
+        System.out.println(d);
+
+
+
+    }
+}
